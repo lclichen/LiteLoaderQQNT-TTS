c3bb4716969e2e50888dfacb87c8bd2c741739b3
molanp <104612722+molanp@users.noreply.github.com>
2024-04-02 16:35:19 +0800

    Fixed: 修复配置文件的一些bug，优化文件存储逻辑 (#9)
    * 修复配置文件读取与设置错误
    使用原生配置文件读取
    修复json非法注释导致的解析错误
    增加发送失败的弹窗提示#3
    
    ---------
    
    Co-authored-by: molanp <782552619@qq.com>

--- src/preload.js
+++ src/preload.js
@@ -1,21 +1,13 @@
-// Electron 主进程 与 渲染进程 交互的桥梁
 const { contextBridge, ipcRenderer } = require("electron");
-
 
 // 在window对象下导出只读对象
 contextBridge.exposeInMainWorld("text_to_speech", {
-    getSettings: () => ipcRenderer.invoke(
-        "LiteLoader.text_to_speech.getSettings"
-    ),
-    setSettings: content => ipcRenderer.invoke(
-        "LiteLoader.text_to_speech.setSettings",
-        content
-    ),
-    openSettings: () => ipcRenderer.invoke(
-        "LiteLoader.text_to_speech.openSettings"
-    ),
+    // 读取全部配置
+    getOptions: () => ipcRenderer.invoke("LiteLoader.text_to_speech.getOptions"),
+    // 打开配置文件所在文件夹/打开配置文件
+    openOptions: () => ipcRenderer.invoke("LiteLoader.text_to_speech.openOptions"),
     // 保存渲染进程获取的数据到数据目录下
     saveFile: (fileName, fileData) => ipcRenderer.invoke('LiteLoader.text_to_speech.saveFile', fileName, fileData),
     // 转换本地文件格式并保存到数据目录下
     convertAndSaveFile: (filePath) => ipcRenderer.invoke('LiteLoader.text_to_speech.convertAndSaveFile', filePath)
-});
+}, { readonly: true });
